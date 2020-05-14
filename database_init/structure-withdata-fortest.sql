97be350bf218cce41602d777c800d6a8de8282cf
Mara <maraanrodriguez@gmail.com>
2020-05-14 19:32:18 -0300

    true-false

--- database_init/structure-withdata-fortest.sql
+++ database_init/structure-withdata-fortest.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `delilah` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `delilah`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: delilah
@@ -34,8 +32,18 @@ CREATE TABLE `details` (
   KEY `products_id` (`products_id`),
   CONSTRAINT `details_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
   CONSTRAINT `details_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `details`
+--
+
+LOCK TABLES `details` WRITE;
+/*!40000 ALTER TABLE `details` DISABLE KEYS */;
+INSERT INTO `details` VALUES (1,1,3,5),(2,1,4,2),(3,2,3,5),(4,2,4,2),(5,3,1,5),(6,3,4,2),(7,4,1,5),(8,4,4,2),(9,5,1,4),(10,5,4,1);
+/*!40000 ALTER TABLE `details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders`
@@ -51,11 +59,22 @@ CREATE TABLE `orders` (
   `description` varchar(255) NOT NULL,
   `amount` decimal(8,2) NOT NULL,
   `paymentmethod` varchar(45) NOT NULL,
+  `removed` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `userid_idx` (`userid`),
   CONSTRAINT `order_user` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,4,'on way','Detalle: wrap x5--fries x2--',95.00,'creditCard',0),(2,4,'cancel','Detalle: wrap x5--fries x2--',95.00,'creditCard',1),(3,4,'new','Detalle: burger x5--fries x2--',95.00,'creditCard',0),(4,3,'cancel','Detalle: burger x5--fries x2--',95.00,'creditCard',0),(5,13,'new','Detalle: burger x4--fries x1--',70.00,'Cash',1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -70,11 +89,21 @@ CREATE TABLE `products` (
   `description` varchar(255) NOT NULL,
   `cost` decimal(8,2) NOT NULL,
   `image` varchar(60) DEFAULT NULL,
-  `status` varchar(45) NOT NULL DEFAULT 'active',
+  `removed` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'burger','bacon and cheese',15.00,'https://cutt.ly/7yeH74c',0),(3,'wrap','grilled vegetables',15.00,'https://cutt.ly/lypqbx4',0),(4,'fries','whit cheddar',10.00,'https://cutt.ly/zyh58Oo',0),(5,'burger','veggie',10.00,'undefined',1),(6,'burger','lamb',50.00,'undefined',0),(7,'burger','lamb',50.00,'undefined',0),(8,'pizza','muzza',40.00,'undefined',0),(9,'pizza','pepperoni',55.00,'',0),(10,'milkshake','milkshake',5.00,'...',0),(11,'sandwich','cheese',5.00,' ',0),(12,'coffee','whit milk',10.00,'...',0);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -93,11 +122,22 @@ CREATE TABLE `users` (
   `address` varchar(45) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(45) NOT NULL DEFAULT 'noadmin',
+  `removed` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (3,'admin','admin','admin','admin@dmin.com','341265365','admin','S3crEt*','admin',0),(4,'marita','Mara','Rodriguez','me@marod.com','3412276654','Corrientes 1344','S3cr37','noadmin',0),(6,'carito','Carolina','Papes','caropapes@me.com','341224856','Corrientes 1600','P3145sa','noadmin',0),(7,'mausita','Mausita','Notiene','mausis@me.com','3415561234','Catamarca 2760','65mnb22','noadmin',0),(8,'jabito','Fabio','Chummi','jabo@me.com','341564646','9 de julio 1450','6546mms','noadmin',1),(10,'gordoalvaro','Alvaro','Chummiento','alvaro@me.com','34133146','Zona sur','456456456','noadmin',0),(12,'mariano182','Mariano','Fernandez','undefined','341331564','Corrientes y Salta','eer456456','noadmin',0),(13,'morella','More','Pais','more@me.com','3412278989','9 de Julio 2145','g65se2','admin',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -108,4 +148,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-10 13:59:38
+-- Dump completed on 2020-05-14 19:24:29
